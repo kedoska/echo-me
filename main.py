6657d56bcb0cf46d79fade4976d27013263a059b
Marco Casula <marco.casula@gmail.com>
2023-04-10 14:34:28 +0200

    ref transcribe

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 from dotenv import load_dotenv
-from transcribe import transcribe
+from transcribe import transcribe, convert_ogg_to_mp3, openai_transcribe
 
 load_dotenv()
 
@@ -17,4 +17,9 @@ if __name__ == '__main__':
         raise Exception(f"File {ogg_file_path} does not exist.")
 
     mp3_file_path = ogg_file_path.replace(".ogg", ".mp3")
-    transcribe(ogg_file_path, mp3_file_path)
+    convert_ogg_to_mp3(ogg_file_path, mp3_file_path)
+
+    text_file_path = ogg_file_path.replace(".ogg", ".txt")
+
+    transcribe(mp3_file_path, text_file_path, transcribe_driver=openai_transcribe)
+
--- transcribe/__init__.py
+++ transcribe/__init__.py
@@ -3,17 +3,28 @@ import openai
 from pydub import AudioSegment
 
 
-def transcribe(ogg_file_path: str, mp3_file_path: str):
+def openai_transcribe(mp3_file_path: str, api_key: str) -> str:
+    try:
+        audio_file = open(mp3_file_path, "rb")
+        transcript = openai.Audio.transcribe("whisper-1", audio_file, api_key=api_key)
+        return transcript.text
+    except Exception as e:
+        print(e)
+
+
+def convert_ogg_to_mp3(ogg_file_path: str, mp3_file_path: str):
     try:
         AudioSegment.from_ogg(ogg_file_path).export(mp3_file_path, format="mp3")
+    except Exception as e:
+        print(e)
 
-        audio_file = open(mp3_file_path, "rb")
-        transcript = openai.Audio.transcribe("whisper-1", audio_file, api_key=os.getenv("OPENAI_API_KEY"))
 
-        text_file_path = ogg_file_path.replace(".ogg", ".txt")
+def transcribe(mp3_file_path: str, text_file_path: str, transcribe_driver=openai_transcribe):
+    try:
+        text = transcribe_driver(mp3_file_path, os.getenv("OPENAI_API_KEY"))
 
         with open(text_file_path, "w") as f:
-            f.write(transcript.text)
+            f.write(text)
     except Exception as e:
         print(e)
     finally:
